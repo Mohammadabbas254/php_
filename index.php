439d31053dfbaf8850db703033c208368fd3cebf
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-02-18 22:57:51 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php learning</title>
+</head>
+<body>
+    <div class="container">
+        first php code
+    <?php
+    echo "heeelllooo printed using php";
+    // single line comment
+
+    /*
+    multi
+    line
+    comment
+    */
+
+    // Variables in php
+
+    $var1 = 34;
+    $var2 = 55;
+
+    echo $var1;
+    echo ' ';
+
+    echo $var2;
+    echo ' ';
+    echo $var1*$var2;
+
+    // -- Operators in php --
+
+    // Arthimetic Operators
+    echo "<br>";
+    echo "The value of var1 + var2 is ";
+    echo $var1+$var2;
+    // Assignment Operators
+    // Comparison Operator
+    // Increment/ Devrement Operator
+    // Logical Operator
+
+
+
+    ?>
+    </div>
+</body>
+</html>
