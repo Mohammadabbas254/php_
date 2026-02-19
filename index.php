06cbbc39ef053bc66c669698f05bf7bd7d528592
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-02-19 11:14:45 +0530

    worked on different types of operatiors in php

--- index.php
+++ index.php
@@ -20,25 +20,81 @@
 
     // Variables in php
 
-    $var1 = 34;
-    $var2 = 55;
+    $var1 = 100;
+    $var2 = 50;
+    echo "<br>";
 
     echo $var1;
     echo ' ';
 
     echo $var2;
     echo ' ';
+    echo "<br>";
+
     echo $var1*$var2;
 
     // -- Operators in php --
+    echo "<h4>Operators in php<h4>";
 
     // Arthimetic Operators
     echo "<br>";
     echo "The value of var1 + var2 is ";
     echo $var1+$var2;
+
+    echo "<br>";
+    echo "The value of var1 - var2 is ";
+    echo $var1-$var2;
+
+    echo "<br>";
+    echo "The value of var1 * var2 is ";
+    echo $var1*$var2;
+
+    echo "<br>";
+    echo "The value of var1 / var2 is ";
+    echo $var1/$var2;
+
     // Assignment Operators
+    echo "<br>";
+
+    $newVar = $var1;
+    echo "The value of newVar is ";
+    echo $newVar;
+    echo "<br>";
+
+    $newVar += 1;
+    echo $newVar;
+    echo "<br>";
+
     // Comparison Operator
+    // echo "<h4>Comparison Operator<h4>";
+    echo "The value of var1 == var2 is ";
+    echo var_dump($var1==$var2);
+    echo "<br>";
+    echo "The value of var1 != var2 is ";
+    echo var_dump($var1!=$var2);
+    echo "<br>";
+    echo "The value of var1 >= var2 is ";
+    echo var_dump($var1>=$var2);
+    echo "<br>";
+    echo "The value of var1 <= var2 is ";
+    echo var_dump($var1<=$var2);
+    echo "<br>";
     // Increment/ Devrement Operator
+    $var3 =1000;
+    $var4 =1000;
+    $var3++;
+    echo "Increment of var3 : ";
+    echo $var3;
+    echo "<br>";
+    $var4--;
+    echo "Decrement of var4 : ";
+    echo $var4;
+
+    echo $var3++;
+    echo $var3--;
+    --$var3;
+    ++$var3; 
+
     // Logical Operator
 
 
