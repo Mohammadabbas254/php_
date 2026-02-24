8d018cd84c1bb92d35f72a772ffc3b168236bc1f
Mohammadabbas254 <mohammadabbasiiit@gmail.com>
2026-02-25 00:04:42 +0530

    Notes App: Create Operation completed successfully, user can now create tasks with description and it will be shown as table

--- 04_crud/index.php
+++ 04_crud/index.php
@@ -1,9 +1,13 @@
 <?php
+
+// INSERT INTO `notes` (`sno`, `title`, `description`, `tstamp`) VALUES ('1', 'php learning', 'this is desc', current_timestamp());
+
+$insert = false;
 //  Connect to the database
-$username = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
-$database = "notes"
+$database = "notes"; 
 
 // Create a connection
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -11,66 +15,119 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn){
     die("Sorry we failed to connect: ". mysqli_connect_error());
 }
+ 
+if ($_SERVER["REQUEST_METHOD"] == 'POST'){
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+
+// Sql query to be executed
+$sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description')";
+$result = mysqli_query($conn, $sql);
+
+if($result){
+    $insert = true;
+}
 else{
-    echo "Connection was successful<br>";
+    echo "error ---> ". 
+    mysqli_error($conn);
+}
 }
 ?>
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Notes App - Notes taking made easy</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Notes App</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact Us</a>
-                </li>
-                
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </div>
         </div>
-        </nav>
-        <div class="container my-4">
-            <h2>Add a Note</h2>
-            <form>
-                <div class="form-group">
-                    <label for="title">Note Title</label>
-                    <input type="text" class="form-control" id="title" name="title">
-                </div>
-                <div class="form-group">
-                    <label for="desc">Note Description</label>
-                    <textarea class="form-control" id="desc" name="desc" rows="3"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary my-3">Add Note</button>
-            </form>
-        </div>
-        <div class="container">
+    </nav>
+
+    <?php 
+    if($insert){
+        echo  "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+  <strong>Success!</strong> Your note has been inserted successfully.
+  <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+</div>";
+    }
+    ?>
+    <div class="container my-4">
+        <h2>Add a Note</h2>
+        <form action="/codeM/04_crud/index.php" method = "POST">
+            <div class="form-group">
+                <label for="title">Note Title</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+            <div class="form-group">
+                <label for="desc">Note Description</label>
+                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary my-3">Add Note</button>
+        </form>
+    </div>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">S.No</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
             <?php
+                $sql = "SELECT * FROM `notes`";
+                $result = mysqli_query($conn, $sql);
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<tr>
+                    <th scope='row'>". $row['sno'] . "</th>
+                    <td>". $row['title'] . "</td>
+                    <td>". $row['description'] . "</td>
+                    <td>Actions</td>
+                </tr>";
+                    
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 
-            ?>
-        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  </body>
 </html>
